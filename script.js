5b5268b68ea9f6d5900a5ffa9d04b29ffd11ece6
Hussein Al-Sayed <hussein.codes.com>
2022-08-18 16:32:46 +0100

    comment function again

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 alert("hello")
+function getNumberOrString(value) {
+    // Convert a string value to a number if possible
+    let number_value = Number(value);
+    if (Number.isNaN(number_value)) {
+      return value
+    } else {
+      return number_value
+    }
+  }
+  
+  
+  
+  document.getElementById('share').addEventListener('click', (event) => {
+    let element_shared = document.getElementById('shared');
+    let new_li = document.createElement('li');
+    new_li.innerText = getNumberOrString(document.getElementById('input').value);
+  
+    element_shared.appendChild(new_li);
+  
+  });
